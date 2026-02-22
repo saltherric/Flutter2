e351c42b0e7615031b09b36f0f5be8b29bb72343
Visal KONG <viisall59@gmail.com>
2026-02-22 20:00:30 +0700

    Task-001-The Color App

--- lib/1_color_app/color_app.dart
+++ lib/1_color_app/color_app.dart
@@ -9,7 +9,25 @@ void main() {
   );
 }
 
-enum CardType { red, blue }
+enum CardType { red, blue, green}
+
+// Global service
+final ColorService colorService = ColorService();
+
+// Service
+class ColorService extends ChangeNotifier {
+  // store all counters in one map
+  final Map<CardType, int> _taps = {CardType.red: 0, CardType.blue: 0, CardType.green: 0};
+
+  int tapCount(CardType type) => _taps[type] ?? 0;
+
+  Map<CardType, int> get taps => _taps;
+
+  void increment(CardType type) {
+    _taps[type] = tapCount(type) + 1;
+    notifyListeners();
+  }
+}
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -20,36 +38,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
-  int redTapCount = 0;
-  int blueTapCount = 0;
-
-  void _incrementRedTapCount() {
-    setState(() {
-      redTapCount++;
-    });
-  }
-
-  void _incrementBlueTapCount() {
-    setState(() {
-      blueTapCount++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _currentIndex == 0
-              ? ColorTapsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-                onRedTap: _incrementRedTapCount,
-                onBlueTap: _incrementBlueTapCount,
-              )
-              : StatisticsScreen(
-                redTapCount: redTapCount,
-                blueTapCount: blueTapCount,
-              ),
+      body: _currentIndex == 0 ? ColorTapsScreen() : StatisticsScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
@@ -73,26 +66,17 @@ class _HomeState extends State<Home> {
 }
 
 class ColorTapsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-  final VoidCallback onRedTap;
-  final VoidCallback onBlueTap;
+  const ColorTapsScreen({super.key});
 
-  const ColorTapsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-    required this.onRedTap,
-    required this.onBlueTap,
-  });
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Color Taps')),
       body: Column(
         children: [
-          ColorTap(type: CardType.red, tapCount: redTapCount, onTap: onRedTap),
-          ColorTap(type: CardType.blue,tapCount: blueTapCount,onTap: onBlueTap),
+          ColorTap(type: CardType.red),
+          ColorTap(type: CardType.blue),
+          ColorTap(type: CardType.green),
         ],
       ),
     );
@@ -101,63 +85,75 @@ class ColorTapsScreen extends StatelessWidget {
 
 class ColorTap extends StatelessWidget {
   final CardType type;
-  final int tapCount;
-  final VoidCallback onTap;
 
-  const ColorTap({
-    super.key,
-    required this.type,
-    required this.tapCount,
-    required this.onTap,
-  });
+  const ColorTap({super.key, required this.type});
 
-  Color get backgroundColor => type == CardType.red ? Colors.red : Colors.blue;
+  Color get backgroundColor {
+    switch (type) {
+      case CardType.red:
+        return Colors.red;
+      case CardType.blue:
+        return Colors.blue;
+      case CardType.green:
+        return Colors.green;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        width: double.infinity,
-        height: 100,
-        child: Center(
-          child: Text(
-            'Taps: $tapCount',
-            style: TextStyle(fontSize: 24, color: Colors.white),
+    return ListenableBuilder(
+      listenable: colorService,
+      builder: (context, child) {
+        final count = colorService.tapCount(type);
+
+        return GestureDetector(
+          onTap: () => colorService.increment(type),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: backgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            width: double.infinity,
+            height: 100,
+            child: Center(
+              child: Text(
+                'Taps: $count',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
 
 class StatisticsScreen extends StatelessWidget {
-  final int redTapCount;
-  final int blueTapCount;
-
-  const StatisticsScreen({
-    super.key,
-    required this.redTapCount,
-    required this.blueTapCount,
-  });
+  const StatisticsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Statistics')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Red Taps: $redTapCount', style: TextStyle(fontSize: 24)),
-            Text('Blue Taps: $blueTapCount', style: TextStyle(fontSize: 24)),
-          ],
-        ),
+      body: ListenableBuilder(
+        listenable: colorService, 
+        builder: (context, child) {
+          final entries = colorService.taps.entries.toList();
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Bonus: if we add a new color type, it shows automatically
+                for (final e in entries) 
+                  Text(
+                    '${e.key} taps: ${e.value}',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
